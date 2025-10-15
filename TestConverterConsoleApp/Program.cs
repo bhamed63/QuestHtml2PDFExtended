7ad608f7895f0c28f17d4d05f05b08fe3ac6d75c
Hamed <b.hamed.63@gmail.com>
2025-10-15 13:00:07 +0330

    console app has been added

---
+++ TestConverterConsoleApp/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HtmlToPdfConverter.Converter;
+using HtmlToPdfConverter.Extractor;
+
+
+var html = System.IO.File.ReadAllText("Files/index_file_for_quest_pdf_test.html");
+var element = HtmlExtractor.Extract(html);
+
+// Act
+var result = PdfConverter.Convert(element);
+
+
+System.IO.File.WriteAllBytes("Files/Test.pdf", result);
